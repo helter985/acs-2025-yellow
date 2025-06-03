5783be187af248f027071329e8272098894062e4
Santiago Moyano <santiago98moy@gmail.com>
2025-06-03 19:50:40 -0300

    agregate item_service

--- backend/app/tests/test_item_service.py
+++ backend/app/tests/test_item_service.py
@@ -6,3 +6,21 @@ from app.schemas.item_schema import ItemCreate, ItemOut, ItemUpdate
 from fastapi import HTTPException
 from bson import ObjectId
 
+@pytest.fixture
+def mock_item():
+    item = Mock()
+    item.id = ObjectId()
+    item.name = "Coca Cola"
+    item.barcode = "1234567890123"
+    item.price = 1.99
+    return item
+
+
+@pytest.fixture
+def mock_item_out(mock_item):
+    return ItemOut(
+        id=str(mock_item.id),
+        name=mock_item.name,
+        barcode=mock_item.barcode,
+        price=mock_item.price
+    )
